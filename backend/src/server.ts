1704b12a347aa2b844538d25c1687ec02644721b
Paulo Roberto <prmorais1302@gmail.com>
2020-10-14 23:08:12 -0300

    Configurando server.ts

---
+++ backend/src/server.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (request, response) => response.json({ message: 'Olá doido.' }));
+
+app.listen(3000, () => console.log('Servidor rodando na porta:', 3000));
