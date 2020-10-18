7004675a4978a16d11b8d372b55e2ba24a6a614c
Paulo Roberto <prmorais1302@gmail.com>
2020-10-18 01:35:42 -0300

    Configurando ESLint e Prettier

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-	return (
-		<h1>Project Manager</h1>
-	);
-}
+const App: React.FC = () => {
+	return <h1>Project Manager</h1>;
+};
 
 export default App;
